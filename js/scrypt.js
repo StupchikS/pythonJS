bad4fcf82c339aaf368a6d7afc454890942784b9
StupchikS <88952411+StupchikS@users.noreply.github.com>
2021-09-02 12:22:13 +0800

    homework3 from 29 aug
    Выполнил задачу 1 из задания от 29 августа, задачу 2 разработал логику, но не работает есть проблемы

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,19 +1,4 @@
-<<<<<<< Updated upstream
-const fizzBuzz=(numbegin, numend) => { 
-        while (numbegin <= numend) numbegin++ 
-            if ( (numbegin % 3) && (numbegin % 5)) {
-             return ('FizzBuzz');
-            }
-            if (numbegin % 3) {
-            return ('Fizz');
-            }
-             if (numbegin % 5) {
-            return ('Buzz');
-            }
-            else  return (numbegin);
-};
-console.log(fizzBuzz);
-=======
+
 //Задача 1 вывести простые числа, делящиеся только на себя
 const primeNumber=(n)=>{
    let result = "1";    //еденицу сразу вписываем, ее проверять смысла нет
@@ -72,4 +57,4 @@ const addNumber=(num)=>{
    
    }
    console.log(addNumber(38));
->>>>>>> Stashed changes
+
